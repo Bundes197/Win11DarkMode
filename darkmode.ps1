289d945a206d4bba960d212665e001e586181ca1
Filip Funda <filipfunda@gmail.com>
2025-10-20 12:22:55 +0200

    feat: add toggle for color prevalence in system

--- darkmode.ps1
+++ darkmode.ps1
@@ -1,11 +1,27 @@
 cls
 
+[PSConstant]$DARK_MODE = 0
+[PSConstant]$LIGHT_MODE = 1
+
+
 function Show-Error {
     $ScriptName = Split-Path -Leaf $PSCommandPath
     Write-Host "Error: Unable to modify registry value." -ForegroundColor Red
     Write-Host "Details: $($_.Exception.Message)" -ForegroundColor DarkRed
     Write-Host "Try unblocking the $($ScriptName) (Right-click on file -> Properties -> Unblock)."
     Write-Host "Or try running $($ScriptName) as administrator."
+}
+
+function Execute-Deactivate-Watermark {
+    try {
+        # Turn off 'Activate Windows' watermark
+        Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name PaintDesktopVersion -Value 0 -ErrorAction Stop
+
+        Write-Host "'Activate Windows' watermark has been turned off." -ForegroundColor Green
+    } catch {
+        Show-Error
+    }
+    
 }
 
 function Deactivate-Watermark {
@@ -15,19 +31,6 @@ function Deactivate-Watermark {
     if ($AnswerWatermark -eq "y") {
         Execute-Deactivate-Watermark
     }
-}
-
-function Execute-Deactivate-Watermark {
-    try {
-        # Turn off 'Activate Windows' watermark
-        Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name PaintDesktopVersion -Value 0 -ErrorAction Stop
-
-        Write-Host "'Activate Windows' watermark has been turned off." -ForegroundColor Green
-    }
-    catch {
-        Show-Error
-    }
-    
 }
 
 function Set-Mode {
@@ -50,8 +53,23 @@ function Set-Mode {
         Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name AppsUseLightTheme -Value $ModeValue -ErrorAction Stop
 
         Write-Host "$($ModeName) mode has been successfully set." -ForegroundColor Green
+    } catch {
+        Show-Error
     }
-    catch {
+}
+
+function Toggle-Accent-Color-Prevalence {
+    $accColorVal = Get-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name ColorPrevalence
+    
+    try {
+        if ($accColorVal -eq 0) {
+            Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name ColorPrevalence -Value 1
+            Write-Host "Theme color prevalence has been successfully turned on." -ForegroundColor Green
+        } else {
+            Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name ColorPrevalence -Value 0
+            Write-Host "Theme color prevalence has been successfully turned off." -ForegroundColor Green
+        }
+    } catch {
         Show-Error
     }
 }
@@ -62,7 +80,7 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
     exit;
 }
 
-$ModeValue = 0 # Default value for dark mode
+$ModeValue = $DARK_MODE # Set dark mode as default
 
 Write-Host ""
 Write-Host "Welcome to Win11DarkMode. What would you like to do?"
@@ -71,26 +89,28 @@ Write-Host ""
 Write-Host "[1] Set Dark mode"
 Write-Host "[2] Set Light mode"
 Write-Host "[3] Turn off 'Activate Windows' watermark"
-Write-Host "[4] Exit"
+Write-Host "[4] Toggle accent color prevalence in system"
+Write-Host "[5] Exit"
 
 Write-Host ""
-Write-Host "(1,2,3,4)"
-Write-Host ""
 
-$AnswerVal = Read-Host "Answer"
+$AnswerVal = Read-Host "Please choose an option (1,2,3,4,5)"
 
 if ($AnswerVal -eq "1") {
     Deactivate-Watermark
-    Set-Mode 0
+    Set-Mode $DARK_MODE
 
 } elseif ($AnswerVal -eq "2") {
     Deactivate-Watermark
-    Set-Mode 1
+    Set-Mode $LIGHT_MODE
 
 } elseif ($AnswerVal -eq "3") {
     Execute-Deactivate-Watermark
 
 } elseif ($AnswerVal -eq "4") {
+    Toggle-Accent-Color-Prevalence    
+
+} elseif ($AnswerVal -eq "5") {
     exit
 
 } else {
